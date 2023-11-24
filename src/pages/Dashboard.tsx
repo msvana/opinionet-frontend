c3f3d30ca7a8bcc820d3db85672754416f76769d
Milos Svana <milos.svana@gmail.com>
2023-11-24 10:43:28 +0100

    Add wordclouds of top words in positive and negative tweets

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import Plot from "react-plotly.js";
 import { useEffect, useState } from "react";
+import WordCloud from "react-d3-cloud";
 
 type SentimentOverview = {
     name: string[];
@@ -9,14 +10,39 @@ type SentimentOverview = {
     mass: number[];
 };
 
+type WordCloudData = {
+    text: string;
+    value: number;
+};
+
+async function fetchWordCloudData(sentiment: string): Promise<WordCloudData[]> {
+    const request = await fetch(`http://localhost:8000/wordcloud/${sentiment}`);
+    const response = await request.json();
+    return response.words;
+}
+
+async function fetchSentimentOverview(): Promise<SentimentOverview> {
+    const request = await fetch("http://localhost:8000/sentiment-overview");
+    const response = await request.json();
+    return response;
+}
+
 function Dashboard() {
     const [sentimentOverview, setSentimentOverview] = useState<SentimentOverview | null>(null);
+    const [wordsPositive, setWordsPositive] = useState<WordCloudData[]>([]);
+    const [wordsNegative, setWordsNegative] = useState<WordCloudData[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
 
     const fetchData = async () => {
-        const request = await fetch("http://localhost:8000/sentiment-overview");
-        const response = await request.json();
-        setSentimentOverview(response);
+        const sentimentOverviewData = await fetchSentimentOverview();
+        setSentimentOverview(sentimentOverviewData);
+
+        const positiveWordcloudData = await fetchWordCloudData("positive");
+        setWordsPositive(positiveWordcloudData);
+
+        const negativeWordcloudData = await fetchWordCloudData("negative");
+        setWordsNegative(negativeWordcloudData);
+
         setLoading(false);
     };
 
@@ -27,75 +53,97 @@ function Dashboard() {
 
         setLoading(true);
         fetchData().catch(console.log);
-    }, [sentimentOverview, loading]);
+    }, [sentimentOverview, wordsPositive, loading]);
 
     return (
         <>
             <div className="row">
-                <div className="col p-3 bg-light border">
-                    <h1>Topic sentiment</h1>
-                    <p>
-                        The graph shows average sentiment, level of positive sentiment, level of
-                        negative sentiment and discussion volume. The greater the yellow square, the
-                        more posts were published on the topic.
-                    </p>
-                    <div className="text-center">
-                        <Plot
-                            className="sentiment-overview"
-                            data={[
-                                {
-                                    x: sentimentOverview?.name,
-                                    y: sentimentOverview?.conNeg,
-                                    type: "bar",
-                                    marker: { color: "#dc3545" },
-                                    name: "Negative sentiment",
-                                },
-                                {
-                                    x: sentimentOverview?.name,
-                                    y: sentimentOverview?.conPos,
-                                    type: "bar",
-                                    marker: { color: "#28a745" },
-                                    name: "Positive sentiment",
-                                },
-                                {
-                                    x: sentimentOverview?.name,
-                                    y: sentimentOverview?.sentMean,
-                                    marker: {
-                                        color: "#ffc107",
-                                        size: sentimentOverview?.mass,
-                                        symbol: "square",
+                <div className="col">
+                    <div className="bg-light border p-3">
+                        <h1>Topic sentiment</h1>
+                        <p>
+                            The graph shows average sentiment, level of positive sentiment, level of
+                            negative sentiment and discussion volume. The greater the yellow square,
+                            the more posts were published on the topic.
+                        </p>
+                        <div className="text-center">
+                            <Plot
+                                className="sentiment-overview"
+                                data={[
+                                    {
+                                        x: sentimentOverview?.name,
+                                        y: sentimentOverview?.conNeg,
+                                        type: "bar",
+                                        marker: { color: "#dc3545" },
+                                        name: "Negative sentiment",
                                     },
-                                    name: "Average sentiment",
-                                    type: "scatter",
-                                    mode: "markers",
-                                },
-                                {
-                                    x: sentimentOverview?.name,
-                                    y: sentimentOverview?.sentMean,
-                                    marker: { color: "#ffffff", size: 2, symbol: "square" },
-                                    type: "scatter",
-                                    showlegend: false,
-                                    mode: "markers",
-                                },
-                            ]}
-                            layout={{
-                                autosize: true,
-                                paper_bgcolor: "#f8f9fa",
-                                plot_bgcolor: "#f8f9fa",
-                                barmode: "relative",
-                                margin: { t: 0, r: 0, l: 39, b: 200 },
-                                xaxis: { tickangle: 45 },
-                            }}
-                            config={{ responsive: true }}
-                        />
+                                    {
+                                        x: sentimentOverview?.name,
+                                        y: sentimentOverview?.conPos,
+                                        type: "bar",
+                                        marker: { color: "#28a745" },
+                                        name: "Positive sentiment",
+                                    },
+                                    {
+                                        x: sentimentOverview?.name,
+                                        y: sentimentOverview?.sentMean,
+                                        marker: {
+                                            color: "#ffc107",
+                                            size: sentimentOverview?.mass,
+                                            symbol: "square",
+                                        },
+                                        name: "Average sentiment",
+                                        type: "scatter",
+                                        mode: "markers",
+                                    },
+                                    {
+                                        x: sentimentOverview?.name,
+                                        y: sentimentOverview?.sentMean,
+                                        marker: { color: "#ffffff", size: 2, symbol: "square" },
+                                        type: "scatter",
+                                        showlegend: false,
+                                        mode: "markers",
+                                    },
+                                ]}
+                                layout={{
+                                    autosize: true,
+                                    paper_bgcolor: "#f8f9fa",
+                                    plot_bgcolor: "#f8f9fa",
+                                    barmode: "relative",
+                                    margin: { t: 0, r: 0, l: 39, b: 200 },
+                                    xaxis: { tickangle: 45 },
+                                }}
+                                config={{ responsive: true }}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div className="row mt-3">
-                <div className="col p-3 bg-light border">
-                    <h1>Word cloud</h1>
-                    <p>Most common words used in posts published from Ostrava</p>
+                <div className="col-lg-6">
+                    <div className="bg-light border p-3">
+                        <h1>Word cloud for Positive tweets</h1>
+                        <p>Most common words used in positive posts published from Ostrava</p>
+                        <WordCloud
+                            data={wordsPositive}
+                            fontSize={(word) => word.value / 2}
+                            rotate={0}
+                            fill="green"
+                        />
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="bg-light border p-3">
+                        <h1>Word cloud for Negative tweets</h1>
+                        <p>Most common words used in negative posts published from Ostrava</p>
+                        <WordCloud
+                            data={wordsNegative}
+                            fontSize={(word) => word.value / 2}
+                            rotate={0}
+                            fill="red"
+                        />
+                    </div>
                 </div>
             </div>
         </>
